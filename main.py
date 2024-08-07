bc2dac582e97a318eea1cb09be8ecccd5cee0822
Reqereddd <152441440+Reqereddd@users.noreply.github.com>
2024-08-07 21:38:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import telebot 
+import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random
 bot = telebot.TeleBot('7262258841:AAHduA6AaHXE2kehIMatUQ81VL2IMYaixts') 
-m_list = ["Шарф", "Футболку"]
-w_list = ["Шарф/Платок", "Футболку","Джинсы/Брюки","Юбку","Платье","Блузку/Рубашку"]
+m_list = ["Шарф", "Футболка"]
+w_list = ["Шарф/Платок", "Футболка","Джинсы/Брюки","Юбка","Платье","Блузка/Рубашка"]
 w_list_weights = [25,25,15,15,10,10]
 channel_id = '-1001522214922'
 def gen_markup():
@@ -41,7 +41,20 @@ def get_user_id(call):
             return True
     except FileNotFoundError as e:
         print(e)
-
+def get_member_id(call):
+    try:
+        f = open('members.txt', 'r')
+        check = str.split(f.read(),',')
+        f.close()
+        if(not (str(call.from_user.id) in check)):
+            f = open('members.txt', 'a')
+            f.write(str(call.from_user.id) + ',')
+            f.close()
+        else:
+            already_get(call=call)
+            return True
+    except FileNotFoundError as e:
+        print(e)
 @bot.callback_query_handler(func = lambda call: call.data in ['Мужчина'])
 def genpriceman(call):
     flag = get_user_id(call = call)
@@ -57,11 +70,11 @@ def genpriceman(call):
 def genpricewoman(call):
     flag = get_user_id(call = call)
     if flag != True:
-        message = "Поздравляею! Вы выиграли " + str((random.choices(w_list, w_list_weights))[0]) + "\nЗабрать можно по адресу: <b>Будапештская 39</b>"
+        message = "Поздравляю! Вы выиграли " + str((random.choices(w_list, w_list_weights))[0]) + "\nЗабрать можно по адресу: <b>Будапештская 39</b>"
         bot.edit_message_text(message_id = call.message.id, parse_mode='HTML', chat_id = call.message.chat.id, text = message,reply_markup=gen_markup_for_adress())
 
 def check_channel_subscribe_message(user_id):
-    return bot.send_message(user_id, "Подпишись на канал и Выиграй приз!",reply_markup=gen_markup())
+    return bot.send_message(user_id, "Не подписаны на канал!",reply_markup=gen_markup())
 
 def pickgender(user_id, chat_id, user_name):
     return bot.edit_message_text(chat_id = chat_id, message_id = check_channel_subscribe_message(user_id).message_id, text = " " + user_name + ", выбери пол👇", reply_markup = gen_markup_for_gender())
@@ -77,7 +90,10 @@ def start(message):
         f = open('users.txt', 'r')
         check = str.split(f.read(),',')
         f.close()
-        if(not (str(user_id) in check)):
+        g = open('members.txt','r')
+        check1 = str.split(g.read(),',')
+        g.close()
+        if(not (str(user_id) in check) and not (str(user_id) in check1)):
             chat_member = bot.get_chat_member(channel_id, user_id)
             if (chat_member.status in ['left','kicked']): 
                 check_channel_subscribe_message(user_id=user_id)
@@ -87,9 +103,13 @@ def start(message):
                         break
             elif(chat_member.status in ['creator','administrator']):
                 pickgender(user_id = user_id, user_name=user_name, chat_id = message.chat.id)
-        else:
-            bot.send_message(chat_id = message.chat.id, parse_mode='HTML', text = "<strong>Вы уже получили приз!</strong>")  
+            elif(chat_member.status in ['member']):
+                get_member_id(message)
+        elif(str(user_id) in check):
+            bot.send_message(chat_id = message.chat.id, parse_mode='HTML', text = "<strong>Вы уже получили приз!</strong>")
+        elif(str(user_id) in check1):
+            bot.send_message(chat_id = message.chat.id, parse_mode='HTML', text = "<strong>Вы уже являетесь подписчиком</strong>")
     except Exception as e: 
         bot.send_message(user_id, e) 
-        
+
 bot.polling()
